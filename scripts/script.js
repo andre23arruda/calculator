c4a8953c8fe74be53a245ff95769aac105b4af7e
André Arruda <33252657+andre23arruda@users.noreply.github.com>
2020-04-26 16:54:41 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,65 @@
+ROUND_VALUE = 1000000
+
+function put_value(key){
+    if (!(document.getElementById("container-number").value)){
+        document.getElementById("number-content").value = ''
+        document.getElementById("container-number").value = true
+    }
+    console.log(document.getElementById("container-number").value)
+    written_keys = document.getElementById("number-content").value
+    document.getElementById("number-content").value = written_keys + key
+}
+
+function put_dot(){
+    written_keys = document.getElementById("number-content").value
+    document.getElementById("number-content").value = written_keys + '.'
+}
+
+function clean_content(){
+    document.getElementById("number-content").value = ''
+    document.getElementById("container-number").value = ''
+}
+
+function clean_last_value(){
+    written_keys = document.getElementById("number-content").value
+    if (written_keys)
+        document.getElementById("number-content").value = written_keys.slice(0, -1)
+}
+
+function operation(key){
+    n = document.getElementById("number-content").value
+    document.getElementById("container-number").value = [n, key]
+    document.getElementById("number-content").value = ''
+}
+
+function result(){
+    n = document.getElementById("container-number").value
+    if(n){
+        n[2] = document.getElementById("number-content").value
+        res = eval('' + n.join(''))
+        if (!(isNaN(res))){
+            document.getElementById("number-content").value = Math.round(res * ROUND_VALUE) / ROUND_VALUE
+        }
+    }
+    else{
+        res = document.getElementById("number-content").value
+        res = res.replace("^", "**");
+        res = res.replace("x", "*");
+        document.getElementById("number-content").value = ''
+        if (!(isNaN(eval(res)))){
+            document.getElementById("number-content").value = Math.round(eval(res) * ROUND_VALUE) / ROUND_VALUE
+        }
+    }
+    console.log(res)
+    document.getElementById("container-number").value = null
+}
+
+document.getElementById("number-content").select()
+var input = document.getElementById("body");
+input.addEventListener("keydown", function() {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        result();
+    }
+});
+
